a2aa1fc3cafc0153054252211a384e82beb1dcaf
laurence-x <lpemailx@gmail.com>
2023-01-17 11:05:49 +0200

    Create TpReg.ts

---
+++ TpReg.ts
@@ -0,0 +1,20 @@
+export type cT = {
+	Nm: React.RefObject<HTMLInputElement>
+	Em: React.RefObject<HTMLInputElement>
+	Em2: React.RefObject<HTMLInputElement>
+	Pw: React.RefObject<HTMLInputElement>
+	Pw2: React.RefObject<HTMLInputElement>
+	ms: React.RefObject<HTMLParagraphElement>
+	iB: React.RefObject<HTMLInputElement>
+	rD: React.RefObject<HTMLDivElement>
+	nvg: (arg: string) => void
+}
+
+export type sendT = {
+	Nm: React.RefObject<HTMLInputElement>
+	Em: React.RefObject<HTMLInputElement>
+	Pw: React.RefObject<HTMLInputElement>
+	ms: React.RefObject<HTMLParagraphElement>
+	rD: React.RefObject<HTMLDivElement>
+	nvg: (arg: string) => void
+}
